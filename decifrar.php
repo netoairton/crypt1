0bc8a5d455c33c40f636817f0d9e134a1b10172e
Airton Neto <netoairton@yahoo.com.br>
2019-08-01 17:07:58 -0300

    cifra/decifrar

---
+++ decifrar.php
@@ -0,0 +1,52 @@
+<?php
+if(!isset($_GET["data"]) || !isset($_GET["sec"])){
+    echo 'Parametros Invalidos';
+}else{
+    $data=$_GET["data"];
+    $sec=$_GET["sec"];
+
+    $num_saida='';
+    $saida='';
+    $letter_array=array('a','b','c','d','e','f','g','h','i','j','k','l','m',
+    'n','o','p','q','r','s','t','u','v','w','x','y','z');
+
+    $myfile = fopen($sec, "r") or die("Unable to open file!");
+    $file_txt=fread($myfile,filesize($sec));
+    fclose($myfile);
+
+    echo $file_txt.'<br>';
+
+    $nova_data=str_replace("/", "", $data);
+    echo $nova_data;
+    echo '<hr>';
+    $array_data = str_split($nova_data);
+    $array = str_split($file_txt);
+    $counter=-1;
+    foreach ($array as $char) {
+        if($char!=' '){
+            ++$counter;
+            $rep_counter=$counter%6;
+            $num=$array_data[$rep_counter];
+            $num_saida.=$num;
+            $nova_letra=array_search($char,$letter_array);
+            $mod=($nova_letra-$num)%26;
+            if($mod<0){
+                $nova_letra=$letter_array[26-abs($mod)];
+            }else{
+                $nova_letra=$letter_array[$mod];
+            }
+            
+        }else{
+            $num=' ';
+            $num_saida.=$num;
+            $nova_letra=' ';
+        }
+        $saida.=$nova_letra;
+        echo $char.' / '.$num.' / '.$nova_letra.'<br>';
+        
+    }
+    echo '<hr>';
+    echo $num_saida.'<br>';
+    echo $saida;
+}
+?>
